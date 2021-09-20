d3063fbd6891e9239f20cdc367318152059e2c6e
Zach Martin <zachmartin9001@gmail.com>
2021-09-20 12:31:27 +1200

    Properly linked front and back end for deployment

--- app.js
+++ app.js
@@ -10,17 +10,26 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Accessing the path module
+const path = require("path");
+
+
 // cors
 app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
 // use Routes
 app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
