dd7bf003b299d4a981a6f9faf09f78229b52ee26
Zach Martin <zachmartin9001@gmail.com>
2021-09-21 00:53:20 +1200

    Updated to use dotenv

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+require("dotenv").config()
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      db,
+      process.env.MONGODB_CONNECTION_STRING,
       {
         useNewUrlParser: true
       }
